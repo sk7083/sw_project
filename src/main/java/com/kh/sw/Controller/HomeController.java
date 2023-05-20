94163fd751abf5e613f886dac48aa8d8449ec466
SunWoo <tjsdn7083@naver.com>
2023-05-20 16:50:58 +0900

    홈 컨트롤러 수정

--- src/main/java/com/kh/sw/Controller/HomeController.java
+++ src/main/java/com/kh/sw/Controller/HomeController.java
@@ -2,20 +2,34 @@ package com.kh.sw.Controller;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.kh.sw.service.MemberService;
+import com.kh.sw.vo.MemberVO;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
+	@Autowired
+	MemberService memberService;
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
@@ -23,18 +37,16 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public ModelAndView home(ModelAndView mv) throws Exception{
+//		List <MemberVO> list = memberService.memberLoad();
+//		for(int i = 0; i<list.size(); i++) {
+//			System.out.println(list.get(i) + "");
+//		}
+		mv.setViewName("mainPage/index");
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		return mv;
+	}	
+
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "mainPage/index";
-	}
-	
 
 }
